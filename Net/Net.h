494470358cdec57c850a96bc9ae5c396ab8e134d
Arthur Myronenko <arthur.myronenko@gmail.com>
2018-05-03 10:36:28 +0300

    Initial Commit

---
+++ Net/Net.h
@@ -0,0 +1,19 @@
+//
+//  Net.h
+//  Net
+//
+//  Created by Arthur Myronenko on 5/3/18.
+//  Copyright © 2018 UPTech Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Net.
+FOUNDATION_EXPORT double NetVersionNumber;
+
+//! Project version string for Net.
+FOUNDATION_EXPORT const unsigned char NetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Net/PublicHeader.h>
+
+
