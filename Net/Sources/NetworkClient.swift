0e1f18b3f24a4b436da12c1823784dd393a776c9
Arthur Myronenko <arthur.myronenko@gmail.com>
2018-05-07 11:31:41 +0300

    Inject JSONDecoder instance into NetworkClient

--- Net/Sources/NetworkClient.swift
+++ Net/Sources/NetworkClient.swift
@@ -29,6 +29,7 @@ public class NetworkClient: NetworkClientProtocol {
   private let baseURL: URL
   private let plugin: CompositePlugin
   private let session: URLSession
+  private let jsonDecoder: JSONDecoder
   private let errorParser: ErrorParser
   private let log: ErrorLogger
 
@@ -36,12 +37,14 @@ public class NetworkClient: NetworkClientProtocol {
     baseURL: URL,
     plugins: [NetworkPlugin] = [],
     session: URLSession = URLSession(configuration: URLSessionConfiguration.default),
+    jsonDecoder: JSONDecoder = JSONDecoder(),
     errorParser: @escaping ErrorParser = defaultErrorParser,
     logger: @escaping ErrorLogger = defaultErrorLogger
     ) {
     self.baseURL = baseURL
     self.plugin = CompositePlugin(plugins: plugins)
     self.session = session
+    self.jsonDecoder = jsonDecoder
     self.errorParser = errorParser
     self.log = logger
   }
@@ -66,7 +69,7 @@ public class NetworkClient: NetworkClientProtocol {
   /// Builds a `URLRequest` by provided `TargetType`.
   ///
   /// - Parameter target: `TargetType` that is used to build the request.
-  /// - Returns: `SignalProducer` that emits a request or `serializationError`.
+  /// - Returns: `Observable` that emits a request or `serializationError`.
   private func buildRequest<T: TargetType>(from target: T) -> Observable<URLRequest> {
     return Observable.deferred { [weak self] () -> Observable<URLRequest> in
       guard let `self` = self else { return .empty() }
@@ -81,10 +84,11 @@ public class NetworkClient: NetworkClientProtocol {
       }.catchError { error in Observable.error(NetworkError.serializationError(message: "\(error)")) }
   }
 
-  /// Executes request
+  /// Executes the request
   ///
   /// - Parameter request: `URLRequest` to execute.
-  /// - Returns: `SignalProducer` with a received `Response`. All errors are mapped into `NetworkError.unknown`.
+  /// - Returns: `Observable` with a fetched `Response`. All errors are mapped into `NetworkError.unknown`.
+  /// If status is 401 emits `NetworkError.unathorized`.
   private func executeRequest(_ request: URLRequest) -> Observable<NetworkResponse> {
     return session.rx.response(request: request)
       .catchError { error in Observable.error(NetworkError.unknown(message: "\(error)")) }
@@ -98,10 +102,15 @@ public class NetworkClient: NetworkClientProtocol {
       .map { response, data in NetworkResponse(statusCode: response.statusCode, data: data) }
   }
 
+  /// Parses object into the `Object.Type`.
+  ///
+  /// - Parameters:
+  ///   - objectType: `Decodable` type of the object to parse the response.
+  ///   - response: `NetworkResponse` to parse the object.
+  /// - Returns: `Observable` of the `Object`. If `Object` couldn't be created tries to parse the error.
   private func parseObject<Object: Decodable>(_ objectType: Object.Type, from response: NetworkResponse) -> Observable<Object> {
-    let decoder = JSONDecoder()
     do {
-      let parsedObject = try decoder.decode(Object.self, from: response.data)
+      let parsedObject = try self.jsonDecoder.decode(Object.self, from: response.data)
       return Observable.just(parsedObject)
     } catch {
       self.log("Couldn't parse model: \(error)")
@@ -110,15 +119,14 @@ public class NetworkClient: NetworkClientProtocol {
     }
   }
 
-  /// Tries to parse response as `ErrorResponse`.
+  /// Tries to parse response using provided error parser.
   ///
   /// - Parameter response: `Response` object to parse.
   /// - Returns: `apiError` if could parse error from `response` or status code is unsuccessfull,
   /// `serializationError` if status code is successfull,
   private func tryParseError(from response: NetworkResponse) -> NetworkError {
-      let decoder = JSONDecoder()
       do {
-        return try errorParser(decoder, response)
+        return try errorParser(jsonDecoder, response)
       } catch {
         log("Couldn't init ErrorResponse from response. Will try to parse generic JSON from response")
       }
